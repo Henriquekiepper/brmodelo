4d934608c229a539031d9176dd4ad6e1767ee748
Henriquekiepper <126527912+Henriquekiepper@users.noreply.github.com>
2023-06-23 14:02:12 -0300

    Create .sql

---
+++ .sql
@@ -0,0 +1,13 @@
+CREATE TABLE lojas.produtos (
+                produto_id NUMERIC(38) NOT NULL,
+                nome VARCHAR(255) NOT NULL,
+                preco_unitario NUMERIC(10,2),
+                detalhes BYTEA,
+                imagem BYTEA,
+                imagem_mime_type VARCHAR(512),
+                imagem_arquivo VARCHAR(512),
+                imagem_charset VARCHAR(512),
+                imagem_ultima_atualizacao DATE,
+                
+                CONSTRAINT produtos_pk PRIMARY KEY (produto_id)
+);
