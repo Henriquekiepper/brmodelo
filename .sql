ed8981023bd94c4ad6762e03a6a50d0925ecd8cb
Henriquekiepper <126527912+Henriquekiepper@users.noreply.github.com>
2023-06-23 14:20:36 -0300

    Update .sql

--- .sql
+++ .sql
@@ -1,16 +1,4 @@
-CREATE TABLE lojas.produtos (
-                produto_id NUMERIC(38) NOT NULL,
-                nome VARCHAR(255) NOT NULL,
-                preco_unitario NUMERIC(10,2),
-                detalhes BYTEA,
-                imagem BYTEA,
-                imagem_mime_type VARCHAR(512),
-                imagem_arquivo VARCHAR(512),
-                imagem_charset VARCHAR(512),
-                imagem_ultima_atualizacao DATE,
-                
-                CONSTRAINT produtos_pk PRIMARY KEY (produto_id)
-);
+
 CREATE TABLE usuario 
 ( 
  id_usuario INT PRIMARY KEY,  
